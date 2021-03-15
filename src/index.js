46a1fe415bce634d6839a5863f9bd8f968d50f0e
kisli-igor <kisiigor@gmail.com>
2021-03-15 23:20:53 +0200

    init: brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let openingBrackets = bracketsConfig( (b) => b[0]);
+    let closingBrackets = bracketsConfig( (b) => b[1]);
+    let stack = [];
+
+    for (let currentChar of str)
+    {
+        let isOpeningBracket = openingBrackets.includes(currentChar)
+        let isClosingBracket = closingBrackets.includes(currentChar)
+        let isCorrespondingClosingBracket = isOpeningBracket && isClosingBracket
+        let notABracket = !isOpeningBracket && !isClosingBracket
+
+        if(notABracket) continue;
+        else if(isOpeningBracket){
+            if (isCorrespondingClosingBracket){
+                let previousBracket = stack.pop()
+
+                if (previousBracket !== undefined){
+                    if (currentChar !== previousBracket){
+                        stack.push(previousBracket)
+                        stack.push(currentChar)
+                    }
+                } else {
+                        stack.push(currentChar)
+                }
+            } else {
+                let correspondingClosingBracket = closingBrackets[openingBrackets.indexOf(currentChar)]
+                stack.push(correspondingClosingBracket)
+            }
+        } else if (isClosingBracket){
+            let expectedClosingBracket = stack.pop()
+            if (currentChar !== expectedClosingBracket) return false;
+        }
+    }
+    return stack.length === 0;
 }
